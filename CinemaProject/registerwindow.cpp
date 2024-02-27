bc891fdc569f7a1efa2c937633fce0ae4acec695
RawanEldeep <rawaneldeep@aucegypt.edu>
2024-02-28 00:13:41 +0200

    Retrieved all the data entered by the user in variables

--- CinemaProject/registerwindow.cpp
+++ CinemaProject/registerwindow.cpp
@@ -1,6 +1,8 @@
 #include "registerwindow.h"
 #include "ui_registerwindow.h"
 #include "users.h"
+#include <QString>
+
 
 #include <QDate>
 
@@ -21,6 +23,44 @@ RegisterWindow::~RegisterWindow()
 
 void RegisterWindow::on_RegisterButton_clicked()
 {
+    QString Username =  ui->UserLineEdit->text();
+    QString Password = ui->PassLineEdit->text();
+    QString ReTypedPassword =  ui->RetypeLineEdit->text();
+    QString Month = ui->MonthComboBox->currentText();
+    QString Day = ui->DayComboBox->currentText();
+    QString year = ui->YearComboBox->currentText();
+    if(ui->MaleRadioButton->isChecked()){
+        QString Male = "Male";
+    }
+    if(ui->FemaleRadioButton->isChecked()){
+        QString Female = "Female";
+    }
+
+    if(ui->UserRadioButton->isChecked()){
+        QString User = "User";
+    }
+    if(ui->AdminRadioButton->isChecked()){
+        QString Admin = "Admin";
+    }
+    if(ui->ActionCheckBox->isChecked()){
+        QString ActionGenre = "Action";
+    }
+    if(ui->RomanceCheckBox->isChecked()){
+        QString RomanceGenre = "Romance";
+    }
+    if(ui->ComedyCheckBox->isChecked()){
+        QString ComedyGenre = "Comedy";
+    }
+    if(ui->DramaCheckBox->isChecked()){
+        QString DramaGenre = "Drama";
+    }
+    if(ui->HorroCheckBox->isChecked()){
+        QString HorrorGenre = "Horrot";
+    }
+    if(ui->OtherCheckBox->isChecked()){
+        QString Other = "Other";
+    }
+    int Year = year.toInt();
 
 }
 
