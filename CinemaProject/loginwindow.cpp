03f88652665e672012010f6b6cfd664a0079f0a1
yomna <yomnaothman@aucegypt.edu>
2024-02-27 21:31:49 +0200

    Fixed Login Window (+ Allowed Error Message to Show up if Username or Password didn't match))

--- CinemaProject/loginwindow.cpp
+++ CinemaProject/loginwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_loginwindow.h"
 #include "welcomewindow.h"
 #include "registerwindow.h"
+#include "users.h"
 
 
 
@@ -21,7 +22,26 @@ LoginWindow::~LoginWindow()
 
 void LoginWindow::on_pushButton_login_clicked()
 {
+    QString username = ui->lineEdit_username->text();
+    QString password = ui->lineEdit_password->text();
 
+    bool found = false;
+    for (int i = 0; i < usersCount; ++i) {
+        if (usernames[i] == username && passwords[i] == password) {
+            found = true;
+            // Successful login, show WelcomeWindow
+            WelcomeWindow *welcomeWindow = new WelcomeWindow(nullptr);
+            welcomeWindow->show();
+            this->hide();
+            break;
+        }
+    }
+
+    // Show error message if login failed
+    if (!found) {
+        ui->label_error->setText("Wrong username or password!");
+        ui->label_error->setVisible(true);
+    }
 }
 
 
